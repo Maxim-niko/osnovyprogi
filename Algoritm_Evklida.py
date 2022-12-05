02eb8c9585c12f4ed947a407e19824eea8ce9a2f
Maxim-niko <113469959+Maxim-niko@users.noreply.github.com>
2022-12-05 19:55:00 +0300

    Add files via upload

---
+++ Algoritm_Evklida.py
@@ -0,0 +1,10 @@
+a = 13
+b = 169
+while a != b:
+    if a > b:
+        a = a - b
+    else:
+        b = b - a
+        
+print(a)
+    
--- Rasshirenny_algoritm.py
+++ Rasshirenny_algoritm.py
@@ -1,4 +1,3 @@
-
 def gcd_extended(num1, num2):
     if num1 == 0:
         return (num2, 0, 1)
@@ -6,5 +5,6 @@ def gcd_extended(num1, num2):
         div, x, y = gcd_extended(num2 % num1, num1)
     return (div, y - (num2 // num1) * x, x)
 a = gcd_extended(100, 101)
+
 print(f'Делитель равен {a[0]}, x = {a[1]}, y = {a[2]}')
 
