cbf9c8f1f7385033e98efacd3e060bfaa7725dbb
Maxim-niko <113469959+Maxim-niko@users.noreply.github.com>
2022-12-25 21:50:56 +0300

    Add files via upload

---
+++ kosinus_fixed.py
@@ -0,0 +1,33 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+ITERATIONS = 13
+
+
+def my_cos(x):
+    x_pow = x
+    multiplier = 1
+    partial_sum = x
+    for n in range(1, ITERATIONS):
+        x_pow *= x ** 2
+        multiplier *= -1 / (2 * n) / (2 * n)
+        partial_sum += x_pow * multiplier
+
+    return partial_sum
+
+
+if __name__=='__main__':
+    help(my_cos)
+    help(math.cos)
+
+    print(my_cos(0.5))
+    print(math.cos(0.5))
+    vs = np.vectorize(my_cos)
+    print(my_cos, vs)
+    arguments = np.arange(1, 10, 0.01)
+    plt.plot(arguments, np.cos(arguments), linewidth=1.0, color='red', label='косинус')
+    plt.plot(arguments, vs(arguments), linewidth=1.5, color='orange', label='ряд Тейлора')
+    plt.legend()
+    plt.show()
