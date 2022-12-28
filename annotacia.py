58c2f4c47ef77232c0d459a4bfd01ec57f590e6b
Nikolashin Maxim <makssssimnik74@gmail.com>
2022-12-28 11:56:41 +0300

    Аннотация типов

--- annotacia.py
+++ annotacia.py
@@ -1,47 +1,16 @@
 import random
 import math
+from typeguard import typechecked
 
-
-print('Найдём НОД двух чисел')
-a = 210
-b = random.randrange(-100000, 100000)
-
-
-
-def Algoritm_Evklida(b:int) ->int:
-    flag=True
+@typechecked
+def algoritm_Evklida(a:int, b:int)->int:
     if b>=0:
         while b != a:
             if b > a:
                 b = b - a
             else:
                 a = a - b
-        print(b)
-        if b > 1:
-            print: ("НОД")
-        else:
-            print: ("Взаимно простые")
-    else:
-        flag= False
-    return(flag)
+        return b
 
-    
-print('Алгоритм Евклида:', b, Algoritm_Evklida(b))  
-
-
-try:
-     print(Algoritm_Evklida(0 , 9))
-     print('Алгоритм Евклида:', b, Algoritm_Evklida(b))
-except:
-    print("Нецелое число")
-
-try:
-    print(Algoritm_Evklida(-88))
-    print('Алгоритм Евклида:', b, Algoritm_Evklida(b))
-except:
-    print("отрицательное число")
-
-    
-
-     
-
+print(algoritm_Evklida(100, 40))
+print(algoritm_Evklida(100, 40.0)) # Eсли функции дать неправильные параметры, то она вылетит
