55cce4a81b6f081eae23b368bbb031d075792c7f
Maxim-niko <113469959+Maxim-niko@users.noreply.github.com>
2023-03-07 20:40:52 +0300

    Numba

---
+++ velocity_acceleration.py
@@ -0,0 +1,19 @@
+import time
+from numba import njit, prange
+
+cur_time = time.time()
+
+@njit(fastmath=True)
+def simple_numbers(n):
+    for l in prange(2, n+1):
+        simple = True
+        for i in prange(2, l):
+            if l % i == 0:
+                simple = False
+                break
+        if simple:
+            number = l
+
+simple_numbers(60000)
+print(time.time() - cur_time)
+    
